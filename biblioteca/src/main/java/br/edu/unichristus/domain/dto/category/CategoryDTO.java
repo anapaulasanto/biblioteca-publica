fc76d78165b85d6284ba1ea8c8f8c4c21fcf746c
liviasydriao <liviasydriao@gmail.com>
2025-04-16 22:20:21 -0300

    adição da entidade Category

---
+++ biblioteca/src/main/java/br/edu/unichristus/domain/dto/category/CategoryDTO.java
@@ -0,0 +1,91 @@
+package br.edu.unichristus.domain.dto.category;
+
+import br.edu.unichristus.domain.model.Category;
+
+import java.util.Objects;
+
+public class CategoryDTO {
+    private Long id;
+    private String categoryName;
+    private String description;
+    private int categoryCode;
+    private int categoryPopularity;
+
+    public CategoryDTO(Long id, String categoryName, String description, int categoryCode, int categoryPopularity) {
+        this.id = id;
+        this.categoryName = categoryName;
+        this.description = description;
+        this.categoryCode = categoryCode;
+        this.categoryPopularity = categoryPopularity;
+    }
+
+    public CategoryDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCategoryCode() {
+        return categoryCode;
+    }
+
+    public void setCategoryCode(int categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+
+    public int getCategoryPopularity() {
+        return categoryPopularity;
+    }
+
+    public void setCategoryPopularity(int categoryPopularity) {
+        this.categoryPopularity = categoryPopularity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category category)) return false;
+        return Objects.equals(id, category.getId()) &&
+                Objects.equals(categoryName, category.getCategoryName()) &&
+                Objects.equals(description, category.getDescription()) &&
+                categoryCode == category.getCategoryCode() &&
+                categoryPopularity == category.getCategoryPopularity();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, categoryName, description, categoryCode, categoryPopularity);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", bookName='" + categoryName + '\'' +
+                ", description='" + description + '\'' +
+                ", categoryCode=" + categoryCode +
+                ", categoryPopularity=" + categoryPopularity +
+                '}';
+    }
+}
--- biblioteca/src/main/java/br/edu/unichristus/service/BookService.java
+++ biblioteca/src/main/java/br/edu/unichristus/service/BookService.java
@@ -1,14 +1,9 @@
 package br.edu.unichristus.service;
 
 import br.edu.unichristus.domain.dto.book.BookDTO;
-import br.edu.unichristus.domain.dto.user.UserDTO;
-import br.edu.unichristus.domain.dto.user.UserLowDTO;
-import br.edu.unichristus.domain.dto.user.UserRolesDTO;
 import br.edu.unichristus.domain.model.Book;
-import br.edu.unichristus.domain.model.User;
 import br.edu.unichristus.exception.CommonsException;
 import br.edu.unichristus.repository.BookRepository;
-import br.edu.unichristus.repository.UserRepository;
 import br.edu.unichristus.utils.MapperUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,8 +17,8 @@ public class BookService {
     @Autowired
     private BookRepository repository;
 
-    public BookDTO save(BookDTO BookDTO){
-        var bookEntity = MapperUtil.parseObject(BookDTO, Book.class);
+    public BookDTO save(BookDTO bookDTO){
+        var bookEntity = MapperUtil.parseObject(bookDTO, Book.class);
         var savedBook = repository.save(bookEntity);
         return MapperUtil.parseObject(savedBook, BookDTO.class);
     }
