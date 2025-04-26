9bf4111291aef42be6aa07790c6540124dc0a7ae
Ana paula <149811410+anapaulasanto@users.noreply.github.com>
2025-04-26 01:07:59 -0300

    adiciona classes que armazenam os objetos das respostas da API

---
+++ library/src/main/java/br/edu/unichristus/api/googleBooks/Items.java
@@ -0,0 +1,22 @@
+package br.edu.unichristus.api.googleBooks;
+
+public class Items {
+    private VolumeInfo volumeInfo;
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public VolumeInfo getVolumeInfo() {
+        return volumeInfo;
+    }
+
+    public void setVolumeInfo(VolumeInfo volumeInfo) {
+        this.volumeInfo = volumeInfo;
+    }
+}
---
+++ library/src/main/java/br/edu/unichristus/api/googleBooks/VolumeInfo.java
@@ -0,0 +1,44 @@
+package br.edu.unichristus.api.googleBooks;
+
+import java.util.List;
+
+public class VolumeInfo {
+
+    private String title;
+    private List<String> authors;
+    private String publishedDate;
+    private String description;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<String> authors) {
+        this.authors = authors;
+    }
+
+    public String getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(String publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
