47e438dd2cc9eeae6f0e02b0c1aaef7ab6ec689e
marilce <marilce@lesteti.com.br>
2025-05-11 16:53:28 -0300

    Adiciona anotações Swagger ao CategoryController

--- library/src/main/java/br/edu/unichristus/controller/CategoryController.java
+++ library/src/main/java/br/edu/unichristus/controller/CategoryController.java
@@ -8,6 +8,10 @@ import br.edu.unichristus.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 import java.util.List;
 
 @RestController
@@ -16,26 +20,41 @@ public class CategoryController {
     @Autowired
     private CategoryService service;
 
+    @Operation(summary = "Cadastra uma nova categoria | role: [ADMIN]", tags = "Category")
     @PostMapping
     public CategoryDTO save(@RequestBody CategoryDTO category){
         return service.save(category);
     }
 
+    @Operation(summary = "Atualiza uma categoria existente | role: [ADMIN]", tags = "Category")
     @PutMapping
     public CategoryDTO update(@RequestBody CategoryDTO category){
         return service.save(category);
     }
 
+    @Operation(summary = "Retorna todas as categorias | role: [ADMIN, USER]", tags = "Category")
     @GetMapping("/all")
     public List<CategoryDTO> findAll(){
         return service.findAll();
     }
 
+    @Operation(summary = "Retorna uma categoria por ID | role: [ADMIN, USER]", tags = "Category")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Categoria encontrada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Categoria não encontrada"),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor")
+    })
     @GetMapping("/{id}")
     public Category findById(@PathVariable Long id){
         return service.findById(id);
     }
 
+    @Operation(summary = "Exclui uma categoria por ID | role: [ADMIN]", tags = "Category")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Categoria excluída com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Categoria não encontrada"),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor")
+    })
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id){
         service.delete(id);
@@ -44,6 +63,7 @@ public class CategoryController {
     @Autowired
     private BookService bookService;
 
+    @Operation(summary = "Retorna livros de uma categoria | role: [ADMIN, USER]", tags = "Category")
     @GetMapping("/{id}/books")
     public List<BookDTO> getBooksByCategory(@PathVariable("id") Long categoryId) {
         return bookService.findBooksByCategoryId(categoryId);
