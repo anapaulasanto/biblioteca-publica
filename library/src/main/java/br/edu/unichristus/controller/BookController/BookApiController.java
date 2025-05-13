4f66f71be00ac63863e92d1dd3bc7e6a1b5ce61c
Marilce <marilcelopes1@gmail.com>
2025-05-13 19:28:50 -0300

    Adiciona tags do swagger para documendar o endpoint.

--- library/src/main/java/br/edu/unichristus/controller/BookController/BookApiController.java
+++ library/src/main/java/br/edu/unichristus/controller/BookController/BookApiController.java
@@ -5,6 +5,9 @@ import br.edu.unichristus.service.BookService.BookApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import java.util.List;
 
 @RestController
@@ -14,21 +17,25 @@ public class BookApiController {
     private BookApiService service;
 
     // ROTAS PARA LIVROS DA API DO GOOGLE
+    @Operation(summary = "Busca todos os livros da API do Google", tags = "Book (API externa)")
     @GetMapping("/external/all") //ex de busca: http://localhost:8081/api/v1/book/external/all
     public List<BookLowDTO> findAllApi() {
         return service.findAllApi();
     }
 
+    @Operation(summary = "Busca livros da API do Google por título", tags = "Book (API externa)")
     @GetMapping("/external/title/{title}") //ex de busca: http://localhost:8081/api/v1/book/externa/title/world
     public List<BookLowDTO> findByTitle(@PathVariable String title) {
         return service.findByTitle(title);
     }
 
+    @Operation(summary = "Busca livros da API do Google por autor", tags = "Book (API externa)")
     @GetMapping("/external/author/{author}") //ex de busca: http://localhost:8081/api/v1/book/external/author/cury
     public List<BookLowDTO> findByAuthor(@PathVariable String author) {
         return service.findByAuthor(author);
     }
 
+    @Operation(summary = "Busca livros da API do Google por assunto", tags = "Book (API externa)")
     @GetMapping("/external/subject/{subject}") //ex de busca: http://localhost:8081/api/v1/book/external/subject/criminal
     public List<BookLowDTO> findBySubject(@PathVariable String subject) {
         return service.findBySubject(subject);
--- library/src/main/java/br/edu/unichristus/controller/BookController/BookController.java
+++ library/src/main/java/br/edu/unichristus/controller/BookController/BookController.java
@@ -10,6 +10,10 @@ import br.edu.unichristus.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 import java.util.List;
 
 @RestController
@@ -18,26 +22,41 @@ public class BookController {
     @Autowired
     private BookService service;
 
+    @Operation(summary = "Cadastra um novo livro | role: [ADMIN]", tags = "Book")
     @PostMapping
     public BookDTO save(@RequestBody BookDTO book){
         return service.save(book);
     }
 
+    @Operation(summary = "Atualiza um livro existente | role: [ADMIN]", tags = "Book")
     @PutMapping("/{id}")
     public BookDTO update(@PathVariable Long id, @RequestBody BookDTO book){
         return service.save(book);
     }
 
+    @Operation(summary = "Retorna todos os livros | role: [ADMIN, USER]", tags = "Book")
     @GetMapping("/all")
     public List<BookDTO> findAll(){
         return service.findAll();
     }
 
+    @Operation(summary = "Retorna um livro por ID | role: [ADMIN, USER]", tags = "Book")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Livro encontrado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Livro não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor")
+    })
     @GetMapping("/{id}")
     public Book findById(@PathVariable Long id){
         return service.findById(id);
     }
 
+    @Operation(summary = "Exclui um livro por ID | role: [ADMIN]", tags = "Book")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Livro excluído com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Livro não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor")
+    })
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id){
         service.delete(id);
@@ -46,6 +65,7 @@ public class BookController {
     @Autowired
     private ReviewService reviewService;
 
+    @Operation(summary = "Retorna avaliações de um livro | role: [ADMIN, USER]", tags = "Book")
     @GetMapping("/{id}/reviews")
     public List<ReviewDTO> getReviewByBook(@PathVariable Long id) {
         return reviewService.findReviewsByBookId(id);
@@ -54,6 +74,7 @@ public class BookController {
     @Autowired
     private RentalService rentalService;
 
+    @Operation(summary = "Retorna locações de um livro | role: [ADMIN]", tags = "Book")
     @GetMapping("/{id}/rentals")
     public List<RentalDTO> getRentalByBook(@PathVariable Long id) {
         return rentalService.findRentalsByBookId(id);
